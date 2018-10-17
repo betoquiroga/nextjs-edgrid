e236641e5cf1f4f9a0de62301051082162fde352
Beto <beto.m12@gmail.com>
2018-10-17 11:11:15 -0400

    Adds pages folder in order to create our first page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React, {Component, Fragment} from 'react'
+import "../styles/styles.scss"
+
+export default class extends Component {
+  render() {
+    return(
+      <div className="ed-container">
+        <h1>My fisrt page with Next.js and EDgrid</h1>
+      </div>
+    )
+  }
+}
